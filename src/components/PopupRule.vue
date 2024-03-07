a8ea0b527e9fafcc1d8480ce9df5cacf18062576
ndt14 <thinh1420003@gmail.com>
2024-03-08 04:17:23 +0700

    hi

--- src/components/PopupRule.vue
+++ src/components/PopupRule.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="popup" :class="getClassPopup">
     <div class="rule">
-      <h1>Luật chơi</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil provident in deleniti sed mollitia beatae ipsam
-        numquam, excepturi repudiandae neque fuga, fugiat, ducimus voluptatem nesciunt nulla? Deserunt consectetur
-        quibusdam nihil?
-      </p>
-      <button v-on:click="confirm" class="confirm">Đã Hiểu</button>
+      <!-- <h1>Luật chơi</h1> -->
+      <h3>
+        Cậu nghĩ đây là 1 trò chơi ư. Dù cậu có quay xúc xắc được bao nhiêu điểm thì cũng đâu có quan trọng. Hôm nay là
+        ngày của cậu .Chúc cậu luôn tự tin, mạnh mẽ và đầy sức sống. Hãy dám nghĩ lớn, hướng tới mục tiêu và không ngừng
+        phấn đấu. Chúc cậu luôn được trải nghiệm những niềm vui, may mắn và thành công trong cuộc sống.Cảm ơn vì đã xuất
+        hiện trong cuộc sống của tớ.❤️❤️❤️
+      </h3>
+      <audio id="player" autoplay loop>
+        <source src="" type="audio/mp3" />
+      </audio>
+      <button v-on:click="confirm" class="confirm">hiuhiu</button>
     </div>
   </div>
 </template>
@@ -54,7 +58,7 @@ export default {
 }
 .rule {
   width: 350px;
-  height: 200px;
+  height: 300px;
   background-color: #fff;
   top: 50%;
   left: 50%;
@@ -65,9 +69,12 @@ export default {
 .open-popup .rule {
   transform: translate(-50%, -50%) scale(1);
 }
-.rule h1 {
+.rule h3 {
   text-align: center;
   margin-top: 10px;
+  margin-bottom: 10px; /* Khoảng cách dưới 10px */
+  font-family: 'Your Font Name', sans-serif; /* Thay 'Your Font Name' bằng tên font chữ bạn muốn sử dụng */
+  font-size: 16px; /* Kích thước font chữ */
 }
 .rule .confirm {
   cursor: pointer;
@@ -75,11 +82,41 @@ export default {
   margin: 10px;
   transition: all 0.3s ease;
   padding: 10px;
-  border: 1px solid black;
+  /* align-items: center;
+  text-align: center; */
+  border: 1px solid white;
+  /* margin: 0px auto; */
 }
 .rule .confirm:hover {
-  color: white;
-  background-color: black;
+  /* color: white; */
+  background-color: white;
 }
-/* kết hợp css3 */
+@media screen and (max-width: 768px) {
+  .popup {
+    max-width: 80%; /* Thay đổi kích thước popup khi màn hình nhỏ hơn 768px */
+    max-height: 80%; /* Thay đổi kích thước popup khi màn hình nhỏ hơn 768px */
+  }
+}
+
+.rule h3 {
+  text-align: center;
+  font-family: 'Your Font Name', sans-serif;
+  font-size: 16px;
+}
+
+.rule .confirm {
+  display: block;
+  margin: 0 auto; /* Căn giữa nút confirm */
+  padding: 10px 20px;
+  cursor: pointer;
+  background-color: #ffffff;
+  border: 1px solid #000000;
+  border-radius: 5px;
+  transition: all 0.3s ease;
+}
+
+.rule .confirm:hover {
+  background-color: #000000;
+  color: #ffffff;
+}
 </style>
